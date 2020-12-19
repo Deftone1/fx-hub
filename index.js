64d170b91b1b25aacf2ae33749117a7b383bcdb5
Paul Murray <paulmurray85@gmail.com>
2020-12-19 09:21:15 -0800

    touched up jaws page added tech info

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import 'bootstrap/dist/js/bootstrap.bundle';
 
+// Javascript to allow dropdown functionality
 Template.navBarTemplate.events({
     'click .dropdown-toggle': function (e) {
         e.preventDefault();
         $(e.target).find('.dropdown-menu').toggle();
         }
 });
+
+// Jaws Modal Functionality
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
+  
 
