35a98fa704fc087fcc7709bb36da0db44f72feb8
Paul Murray <paulmurray85@gmail.com>
2020-12-28 11:42:07 -0800

    created modal for jp images

--- js/jp.js
+++ js/jp.js
@@ -11,14 +11,6 @@ $('.myImg').click(function(){
     modalImg.attr('src', newSrc);
     captionText.innerHTML = this.alt;
 });
-// var img = document.getElementById("myImg");
-// var modalImg = document.getElementById("img01");
-// var captionText = document.getElementById("caption");
-// img.onclick = function(){
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   captionText.innerHTML = this.alt;
-// }
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
